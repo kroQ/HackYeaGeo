5efe599b48d5e21863b98481d86b22c64c8368e9
KroQ <morvidkrk@gmail.com>
2017-10-29 08:13:33 +0100

    poprawki

--- ui/choose_columns.py
+++ ui/choose_columns.py
@@ -3,6 +3,8 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.checkbox import CheckBox
+from kivy.uix.textinput import TextInput
+from kivy.uix.behaviors import FocusBehavior
 from kivy.core.window import Window
 from data import csv_loadHeader
 from kivy.uix.popup import Popup
@@ -20,6 +22,8 @@ class ChooseColumns(GridLayout):
          for idx, wgt in enumerate(self.check_ref.items()):
             if wgt[1].active:
                 self.selected.append(idx)
+         print(self.street.text)
+         print(self.buildingNumber.text)
 
          self.zButaWjezdzam()
 
@@ -34,6 +38,8 @@ class ChooseColumns(GridLayout):
         self.hhh = {}
         self.selected = []
         self.check_ref = {}
+        self.street = None
+        self.buildingNumber = None
 
         def get_r():
             return self.selected, 4, None
@@ -46,13 +52,27 @@ class ChooseColumns(GridLayout):
         self.hhh = heders
 
         content.cols = 6
-        for i in heders:
+        for i in enumerate(heders):
+            # print(i)
             cbx = CheckBox()
-            self.check_ref["cb" + str(i)] = cbx
+            self.check_ref["cb" + str(i[1])] = cbx
             content.add_widget(cbx)
-            content.add_widget(Label(text=i))
+            content.add_widget(Label(text=str(i[0])+ " " + i[1]))
         btn1 = Button(text="Analysis")
+
+        # txtField = TextInput(hint_text="ulica", input_filter=int)
+        txtField = TextInput(input_filter = 'int', hint_text = 'Ulica: ')
+
+        # txtField = TextInput(hint_text="ulica")
+        txtField2 = TextInput(input_filter='int', hint_text='Nr Domu: ')
+
         content.add_widget(btn1)
+        content.add_widget(Label(text="Wybor ulicy:"))
+        content.add_widget(txtField)
+        content.add_widget(Label(text="Wybor budynku, jesli osobny naglowek"))
+        content.add_widget(txtField2)
+        self.street = txtField
+        self.buildingNumber = txtField2
         btn1.bind(on_press=self.getcheckboxes_active)
         popup.open()
 
