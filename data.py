5e610955006437868bfe913a5901fd5908412d5e
Tomasz Bijata <bijatatomasz@gmail.com>
2017-10-29 08:10:22 +0100

    Hello matching :)

--- data.py
+++ data.py
@@ -3,20 +3,37 @@ from utils import *
 
 def csv_loadHeader(filename, columns=[]):
     with open(filename, encoding="utf-8-sig") as f:
-        return get_elements(f.readline().lower().strip().split(";"), columns)
+        return [e.strip() for e in get_elements(f.readline().lower().strip().split(";"), columns)]
 
 
-def csv_load(filename, skip_first_line=False, columns=[]):
+def csv_load(filename, skip_first_line=False, columns=[], add_id_column=False):
     data = []
     with open(filename, encoding="utf-8-sig") as f:
         if skip_first_line:
             f.readline()
 
         content = f.readlines()
+        counter = 0
         for line in content:
-            data.append(get_elements(line.lower().strip().split(";"), columns))
+            if add_id_column:
+                counter += 1
+                line = str(counter) + ';' + line
+            es = [e.strip() for e in get_elements(line.lower().strip().split(";"), columns)]
+            data.append(es)
 
     return data
+
+def save_ready_csv(data_queue, filename, header=None):
+    with open(filename, "w", encoding="utf-8-sig") as f:
+        if header is not None:
+            f.write(header.strip() + '\n')
+
+        while not data_queue.empty():
+            row = data_queue.get()
+            print("row:", row)
+            str_line = ";".join(row) + '\n'
+            print(filename, "Save:", str_line)
+            f.write(str_line)
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -1,11 +1,40 @@
 from operator import itemgetter
 
+prefixes_to_remove = ['os ', 'os.', 'osiedle ', 'al.', 'al ', 'aleja ', 'aleja.', 'pl ', 'pl. ', 'plac ', 'plac. ',
+                      'płk.', 'gen.', 'gen ', 'prof.', 'prof ', 'ks. ', 'ks ', 'księdza ', 'ksiedza ',
+                      'bp.', 'bp ', 'por.', 'por ', 'mjr.', 'mjr ', 'marsz.', 'marsz ', 'z.', 'o.', 's.',
+                      'b.', 'j.', 'm.', 'w.', 'f.', 'rtm.', 'rtm ', 'abp.', 'jerzego ', 'ignacego ',
+                      'dr.', 'św. ', 'św ', 'ul. ', 'ul ', 'stanisławy', 'teodora', 'księcia', 'konstantego',
+                      'ildefonsa', 'franciszka', 'juliana', 'kornela', 'mikołaja', ' jana', 'władysława', 'ulica',
+                      'romana', 'karola', 'hanny','sandora','gustawa','mieczysława', 'melchiora','wacława','cypriana',
+                      'narcyza','andrzeja','michała', 'juliusza', 'genenerała','e.', 'a.', 'kiejstuta','adolfa',
+                      'jaxy','królowej','piotra','vlastimila', 'mahatmy', 'erazma', 'stanisława', ' i ','ludwika','eugeniusza',
+                      'daniela','maurycego','walerego','feliksa', 'leona', 'lucjana', 'leopolda',]
 
-def get_elements(data, columns):
+def remove_sub_str(to_remove, in_str):
+    for to_r in to_remove:
+        if in_str.startswith(to_r):
+            in_str = in_str[len(to_r):]
+
+    return in_str.strip()
+
+# class Filer(object):
+#     def __init__(self, fun, *args):
+#         self.f = fun
+#         self.args = args
+#
+#     def filter(self, data):
+#         self.fun()
+
+def get_elements(data, columns, filter=None, filter_column_nr=None):
     if isinstance(columns, list):
         if len(columns) > 0:
             return itemgetter(*columns)(data)
         else:
             return data
+
+        # if filter is not None:
+        #     d =
+
     else:
         raise Exception("Columns mu be integer list")
